e7c27545a23cfa6e738e9e4c6118a7a4dda36f27
J. Nathan Matias <natematias@gmail.com>
2012-10-04 13:35:05 -0400

    updated testLoadCSVFiles to include a more declarative style

--- test/unit/usworld_results_test.py
+++ test/unit/usworld_results_test.py
@@ -4,9 +4,26 @@ from usworld_results import *
 
 class USWorldResultsTest(unittest.TestCase):
 
+  def preprocess_article_assertions(self, key, category):
+    for value in [ (key in row) for row in category]:
+      self.assertTrue(value)
+    
+
   def testLoadCSVFiles(self):
     #import pdb;pdb.set_trace()
-    USWorldResults("test/fixtures/")
+   results = USWorldResults("test/fixtures/")
+   categories = [results.sports, results.local]
+
+   [self.assertEqual(19, len(category)) for category in categories]
+
+   [self.preprocess_article_assertions("content_tokens", category) 
+     for category in categories]
+
+   [self.preprocess_article_assertions("headline_tokens", category) 
+     for category in categories]
+
+
+   import pdb; pdb.set_trace()
     
 
 test_common.ALL_TESTS.append(USWorldResultsTest)
