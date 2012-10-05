1121cd9c6a017050fb177ef019d00c667e318b30
J. Nathan Matias <natematias@gmail.com>
2012-10-05 14:54:02 -0400

    cleaned data, created training and evaluation fixtures, created naive bayesian classifier code

--- src/usworld_results.py
+++ src/usworld_results.py
@@ -1,17 +1,71 @@
 import csv
 import nltk
+import re
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
+import random
 
 class USWorldResults:
   def __init__(self, folder):
-    self.sports = [self.preprocess_article(row) for row in csv.DictReader(open(folder + "sports.csv"))]
-    self.local = [self.preprocess_article(row) for row in csv.DictReader(open(folder + "local.csv"))]
+    self.categories = {"sports":None, "local":None}
+    self.category_words = {"content":[], "headline":[]}
+    self.frequencies={"content":nltk.FreqDist(), "headline":nltk.FreqDist()}
 
-  def preprocess_article(self, row):
-    lmtz = WordNetLemmatizer()
-    row["content_tokens"] = [lmtz.lemmatize(w) for w in nltk.wordpunct_tokenize(row["content"]) 
-                              if not w in stopwords.words('english')]
-    row["headline_tokens"] = [lmtz.lemmatize(w) for w in nltk.wordpunct_tokenize(row["headline"])
-                              if not w in stopwords.words('english')]
+    for category in self.categories:
+      self.categories[category] = {}
+      self.categories[category]["documents"] = [self.preprocess_article(row, category) 
+        for row in csv.DictReader(open(folder + category + ".csv")) if row["answer"]=="Yes"]
+
+    self.train_classifier()
+
+  #tokenize and lemmatize incoming content,
+  #set up frequency distributions
+  def preprocess_article(self, row, category):
+    row = self.load_article(row)
+
+    self.frequencies["content"].update(row["content_tokens"])
+    self.frequencies["headline"].update(row["headline_tokens"])
+
+    self.category_words["content"].append((row["content_tokens"], category))
+    self.category_words["headline"].append((row["headline_tokens"], category))
+
     return row
+
+  #for a given CSV row coming in from crowdflower, tokenize, lemmatize, remove stopwords and punct
+  def load_article(self, row):
+    lmtz = WordNetLemmatizer()
+    p = re.compile(r'[-.?!,":;()]')  
+    row["content_tokens"] = [lmtz.lemmatize(w.lower()) for w in nltk.wordpunct_tokenize(row["content"]) 
+                              if (not w in stopwords.words('english') and not p.search(w))]
+    row["headline_tokens"] = [lmtz.lemmatize(w.lower()) for w in nltk.wordpunct_tokenize(row["headline"])
+                              if (not w in stopwords.words('english') and not p.search(w))]
+    return row
+
+  #train a naive bayesian classifier on the training set
+  #TODO: Include headlines, not just content
+  def train_classifier(self):
+    self.classifier = nltk.NaiveBayesClassifier.train(
+                        nltk.classify.util.apply_features(
+                          self.extract_content_features, 
+                          self.category_words["content"]))
+
+  def extract_headline_features(self, wordlist):
+    return self.extract_features(wordlist, "headline")
+
+  def extract_content_features(self, wordlist):
+    return self.extract_features(wordlist, "content")
+
+  def extract_features(self, wordlist, field_name):
+    words = set(wordlist)
+    features = {}
+    for word in self.frequencies[field_name].keys():
+      features['%s_contains(%s)' %(field_name, word)] = (word in words)
+    return features
+
+  #load evaluation dataset
+  def setup_evaluation(self, folder):
+    self.eval_categories = {"sports":None, "local":None}
+    for category in self.eval_categories:
+      self.eval_categories[category] = {}
+      self.eval_categories[category]["documents"] = [self.load_article(row) 
+        for row in csv.DictReader(open(folder + category + ".csv", "rb")) if row["answer"]=="Yes"]
--- test/unit/usworld_results_test.py
+++ test/unit/usworld_results_test.py
@@ -11,19 +11,18 @@ class USWorldResultsTest(unittest.TestCase):
 
   def testLoadCSVFiles(self):
     #import pdb;pdb.set_trace()
-   results = USWorldResults("test/fixtures/")
-   categories = [results.sports, results.local]
-
-   [self.assertEqual(19, len(category)) for category in categories]
-
-   [self.preprocess_article_assertions("content_tokens", category) 
-     for category in categories]
-
-   [self.preprocess_article_assertions("headline_tokens", category) 
-     for category in categories]
-
-
-   import pdb; pdb.set_trace()
+    results = USWorldResults("test/fixtures/training/")
+    results.setup_evaluation("test/fixtures/evaluation/")
+ 
+    #verify that the proper number of rows are imported
+    self.assertEqual(26, len(results.categories["sports"]["documents"]))
+    self.assertEqual(73, len(results.categories["local"]["documents"]))
+ 
+    #test that the headline and content of every row is imported
+    [self.preprocess_article_assertions(field, results.categories[category]["documents"]) 
+      for category in results.categories for field in results.frequencies]
+    import pdb; pdb.set_trace()
+    #results.classifier.classify(results.extract_content_features(results.eval_categories["sports"]["documents"][10]["content"]))
     
 
 test_common.ALL_TESTS.append(USWorldResultsTest)
